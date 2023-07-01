8b12b4b353e9013fc1c859b047a6428796987bb8
fiahamasyatusss <fia.syahadah.ti.21@cic.ac.id>
2023-07-01 12:06:16 +0700

    first commit

---
+++ tugas_latihan2/android/app/src/main/kotlin/com/example/tugas_latihan2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_latihan2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tugas_latihan3/android/app/src/main/kotlin/com/example/tugas_latihan3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_latihan3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
